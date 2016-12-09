d542774d90c3114f0f48556b168f93ccd903c2ed
gdmec07150816 <928167503@qq.com>
2016-12-09 20:05:05 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150816/mymediaplayer/ListView.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.s07150816.mymediaplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.file_item);
+    }
+}
